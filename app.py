0fc269886112e112920a8dc705f8b71fd0d28eda
MILTON VANEGAS DELGADO <mvanegas_usistemas@intep.edu.co>
2026-09-10 14:38:48 -0500

    Archivos iniciales

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="Dashboard De Occidente", layout="wide")
+
+st.title("🚀 Dashboard Operativo - Transportadores de Occidente")
+st.write("Si ves este mensaje, la conexión con Streamlit Cloud fue un éxito.")
+st.write("El próximo paso será conectar la base de datos PostgreSQL.")
